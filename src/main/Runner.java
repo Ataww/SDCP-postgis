44e6e871520f002add8dbd6c0cb2b07986dd6984
Nathanael COURET <nathanael.couret@ensimag.grenoble-inp.fr>
2016-10-06 12:14:56 +0200

    Added argument handling (cf q.11)

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -4,6 +4,8 @@
 package main;
 
 import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import database.Utils;
 
@@ -13,25 +15,34 @@ import database.Utils;
  */
 public class Runner {
 	
+	private static final Logger logger = Logger.getLogger(Runner.class.getName());
+	
 	private Connection connection;
+	String queryArg;
 
 	/**
 	 * 
 	 */
-	public Runner() {
+	public Runner(String arg) {
 		connection = Utils.getConnection();
+		queryArg = arg;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Runner runner = new Runner();
+		if(args.length < 1) {
+			logger.log(Level.SEVERE, "1 argument expected, none received.");
+			return;
+		}
+		Runner runner = new Runner(args[0]);
 		runner.run();
 		runner.tearDow();
 	}
 	
 	public void run() {
+		
 	}
 	
 	public void tearDow() {
